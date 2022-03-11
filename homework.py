0085b25aaf963c215b11b0bb9b192c0535209048
Денис Маресов <frostik1989@yandex.ru>
2022-03-11 17:13:40 +0300

    redact retry time

--- homework.py
+++ homework.py
@@ -28,7 +28,7 @@ PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TOKEN_TELEGRAM")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT")
 
-RETRY_TIME = 60
+RETRY_TIME = 600
 ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
